fa578bce53710fc19d6c7b25846652b794fc0edb
Samuel Ting <tingjs05@gmail.com>
2024-06-05 15:08:04 +0800

    added popup message

--- Assets/Scripts/UI/PageManager.cs
+++ Assets/Scripts/UI/PageManager.cs
@@ -6,13 +6,17 @@ public class PageManager : MonoBehaviour
 {
     [SerializeField] GameObject body;
     [SerializeField] GameObject wipMessage;
+    [SerializeField] GameObject wipPopup;
+    [SerializeField] float wipPopupDuration = 2f;
     [SerializeField] BottomNavigationManager navManager;
+
+    Coroutine coroutine;
 
     // Start is called before the first frame update
     void Start()
     {
         // error check
-        if (body == null || wipMessage == null || navManager == null) return;
+        if (body == null || wipMessage == null || wipPopup == null || navManager == null) return;
 
         // set default active
         body.SetActive(true);
@@ -27,5 +31,20 @@ public class PageManager : MonoBehaviour
         if (body == null || wipMessage == null) return;
         body.SetActive(page == BottomNavigationManager.NavPages.HOME);
         wipMessage.SetActive(page != BottomNavigationManager.NavPages.HOME);
+    }
+
+    public void ShowPopup()
+    {
+        if (wipPopup == null) return;
+        if (coroutine != null) StopCoroutine(coroutine);
+        coroutine = StartCoroutine(ShowPopup(wipPopupDuration));
+    }
+
+    IEnumerator ShowPopup(float duration)
+    {
+        wipPopup.SetActive(true);
+        yield return new WaitForSeconds(duration);
+        coroutine = null;
+        wipPopup.SetActive(false);
     }
 }   
