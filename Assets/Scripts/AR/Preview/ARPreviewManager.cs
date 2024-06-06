43d2a4ba45caa5b20d61f79243e18b8c360cc1a8
Samuel Ting <tingjs05@gmail.com>
2024-06-06 17:26:32 +0800

    added interact method

--- Assets/Scripts/AR/Preview/ARPreviewManager.cs
+++ Assets/Scripts/AR/Preview/ARPreviewManager.cs
@@ -7,6 +7,12 @@ using UnityEngine.XR.ARSubsystems;
 [RequireComponent(typeof(ARSession))]
 public class ARPreviewManager : MonoBehaviour
 {
+    [Header("Interaction")]
+    [SerializeField] float interactionDistance = 1f;
+    [SerializeField] float interactionRange = 0.5f;
+    [SerializeField] LayerMask interactionLayer;
+
+    [Header("References")]
     [SerializeField] ARSession session;
 
     // Start is called before the first frame update
@@ -21,9 +27,39 @@ public class ARPreviewManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // handle inputs by users
+        HandleInputs();
     }
 
+    void HandleInputs()
+    {
+        // check if user is touching the screen, ensure the user is touching the screen
+        if (Input.touchCount <= 0) return;
+        // get touch input
+        Touch touch = Input.GetTouch(0);
+        // try to interact with objects
+        Interact(touch);
+    }
+
+    // methods to handle interactions
+    void Interact(Touch touch)
+    {
+        // only interact when touch phase just begins
+        if (touch.phase != TouchPhase.Began) return;
+
+        // erase lines within range through colliders
+        Collider[] hits = Physics.OverlapSphere(Camera.main.transform.position + (Camera.main.transform.forward * interactionDistance), interactionRange, interactionLayer);
+        // check if there are any collisions
+        if (hits.Length <= 0) return;
+
+        // erase lines hit
+        foreach (Collider hit in hits)
+        {
+            
+        }
+    }
+    
+    // button methods
     public void ResetARSession()
     {
         session?.Reset();
